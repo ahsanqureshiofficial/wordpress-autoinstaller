a9051075d7c086e36c03803afe1e59732f905d52
Ahsan Qureshi <71433563+ahsanqureshiofficial@users.noreply.github.com>
2024-05-13 12:30:58 +0500

    Update firewall rules for litespeed panel

--- openlitespeed-wordpress.sh
+++ openlitespeed-wordpress.sh
@@ -3,5 +3,7 @@
 bash <( curl -sk https://raw.githubusercontent.com/litespeedtech/ls-cloud-image/master/Setup/wpimgsetup.sh )
 ### Regenerate password for Web Admin, Database, setup Welcome Message
 bash <( curl -sk https://raw.githubusercontent.com/litespeedtech/ls-cloud-image/master/Cloud-init/per-instance.sh )
+## Add firewall rules for ls panel
+ufw allow 7080/tcp
 ### Reboot server
 reboot
