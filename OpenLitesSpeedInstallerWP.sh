a58ef1b3ab7378181ffffe96c1fa889cf953fe37
Ahsan Qureshi <71433563+ahsanqureshiofficial@users.noreply.github.com>
2024-05-13 11:30:19 +0500

    Add files via upload

---
+++ OpenLitesSpeedInstallerWP.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+### Install OpenLiteSpeed and WordPress
+bash <( curl -sk https://raw.githubusercontent.com/litespeedtech/ls-cloud-image/master/Setup/wpimgsetup.sh )
+### Regenerate password for Web Admin, Database, setup Welcome Message
+bash <( curl -sk https://raw.githubusercontent.com/litespeedtech/ls-cloud-image/master/Cloud-init/per-instance.sh )
+# Delete the installer file
+rm -rf setup.sh
+### Reboot server
+reboot
